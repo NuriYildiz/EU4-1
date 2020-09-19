c8c05fb27dbcb9c7cc314bf3d3963e9c3122fd00
ferro-sudo <ferro-sudo@users.noreply.github.com>
2020-09-19 12:58:04 +0300

    correction1

--- assignments/day_2/Question8.java
+++ assignments/day_2/Question8.java
@@ -4,15 +4,15 @@ public class Question8 {
 
 	public static void main(String[] args) {
 
-		System.out.println("******           *************      **********");
-		System.out.println("**    **                **          **");
-		System.out.println("**     **               **          **");
-		System.out.println("**      **              **          **");
-		System.out.println("**      **              **          ********");
-		System.out.println("**      **       **     **          **");
-		System.out.println("**     **         **    **          **");
-		System.out.println("**    **           **  **           **");
-		System.out.println("*****               ****            ***********");
+		System.out.println("**********            *******");
+		System.out.println("**                  **");
+		System.out.println("**                 **");
+		System.out.println("**                **");
+		System.out.println("******            **");
+		System.out.println("**                **");
+		System.out.println("**                 **");
+		System.out.println("**                  **");
+		System.out.println("**                    *******");
 		
 	}
 
