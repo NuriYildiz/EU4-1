1158831fef0e079895a28d405eb97173a7ab47f7
ferro-sudo <ferro-sudo@users.noreply.github.com>
2020-10-22 00:50:09 +0300

    21.10.2020

---
+++ assignments/Lab_2/question_3.java
@@ -0,0 +1,19 @@
+package Lab_2;
+
+import java.util.Scanner;
+
+public class question_3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a nonzero integer value:");
+		int num= sc.nextInt();
+		int sum=0;
+		for(int i=1;i<=num;i++) {
+			sum+=i;
+		}System.out.println(sum);
+		sc.close();
+
+	}
+
+}
